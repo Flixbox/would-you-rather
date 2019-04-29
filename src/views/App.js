f984bc16942050817a16203ec283566892115a75
Felix <Felix.Tietjen@txs.de>
2019-04-29 13:33:56 +0200

    feat: theming baseline

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { HashRouter as Router } from 'react-router-dom'
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    },
+    typography: { useNextVariants: true },
+})
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>hello world!</p>
-            </header>
-        </div>
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="App">
+                <header className="App-header">
+                    <p>hello world!</p>
+                </header>
+            </div>
+        </MuiThemeProvider>
     )
 }
 
