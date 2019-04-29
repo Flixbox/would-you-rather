d8252d3b9439d7cd91cc12a93c5cf44dce645614
Felix <Felix.Tietjen@txs.de>
2019-04-29 16:30:10 +0200

    docs: documented shared.js in the actions folder

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,13 @@
 import { getInitialData } from '../helpers/api'
 import { receiveUsers } from '../actions/users'
 
+/**
+ * Dispatched when the app loads.
+ * Fetches all of the question and user data.
+ *
+ * @export
+ * @returns {Promise} This resolves when the user data is retrieved.
+ */
 export function handleInitialData() {
     return dispatch => {
         return getInitialData().then(({ users, questions }) => {
