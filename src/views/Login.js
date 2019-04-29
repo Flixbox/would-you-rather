411e234e2e467795d89676d0f6f6ec24c6289172
Felix <Felix.Tietjen@txs.de>
2019-04-29 17:02:04 +0200

    feat: successfully displayed users

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import Grid from '@material-ui/core/Grid'
+import { Typography } from '@material-ui/core'
+
 class Login extends Component {
     render() {
-        return <div>Login</div>
+        let { users } = this.props.users
+        console.log(this.props)
+
+        if (!users) {
+            return (
+                <Grid container justify="center">
+                    <Typography>No users found!</Typography>
+                </Grid>
+            )
+        }
+
+        return (
+            <Grid container justify="center">
+                {Object.entries(users).map(([id, user]) => (
+                    <Grid item xs={12} m={6} l={3}>
+                        {user.name}
+                    </Grid>
+                ))}
+            </Grid>
+        )
     }
 }
 
-function mapStateToProps({ authedUser, users }) {
+function mapStateToProps({ users }) {
     return {
-        authedUser,
         users,
     }
 }
