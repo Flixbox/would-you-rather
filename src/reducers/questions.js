4f94b76f3c2b61d1cce54ece168984ac545869a3
Felix <Felix.Tietjen@txs.de>
2019-04-30 13:34:03 +0200

    feat: added an algorithm to calculate the user's active question

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -8,9 +8,27 @@ export default (state = {}, action) => {
                 questions: action.questions,
             }
         case RETRIEVE_NEW_QUESTION:
+            for (const key in state.questions) {
+                const question = state.questions[key]
+                // Concatenate the two arrays to get all votes of the question
+                const votes = question.optionOne.votes.concat(question.optionTwo.votes)
+                // Find the first question where the user hasn't voted
+                let hasVoted = false
+                for (const key in votes) {
+                    if (votes[key] === action.authedUser.id) {
+                        hasVoted = true
+                    }
+                }
+                if (!hasVoted) {
+                    return {
+                        ...state,
+                        currentQuestion: key,
+                    }
+                }
+            }
             return {
                 ...state,
-                currentQuestion: 'test',
+                currentQuestion: null,
             }
         default:
             return state
