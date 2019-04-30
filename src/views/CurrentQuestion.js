6d8aea84e9904c956f6b542196a6629e9da2af7c
Felix <Felix.Tietjen@txs.de>
2019-04-30 16:48:15 +0200

    feat: added bottom nav with font awesome icons to the current question view

--- src/views/CurrentQuestion.js
+++ src/views/CurrentQuestion.js
@@ -1,6 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { handleRetrieveNewQuestion } from '../actions/questions'
+
+import classNames from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import {
+    Typography,
+    Button,
+    Avatar,
+    Grid,
+    BottomNavigation,
+    BottomNavigationAction,
+} from '@material-ui/core'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const styles = {
+    main: {},
+    option: {
+        flex: '1 0 auto',
+        width: '50%',
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+}
 
 class CurrentQuestion extends Component {
     componentDidMount = () => {
@@ -18,9 +46,41 @@ class CurrentQuestion extends Component {
     }
 
     render() {
-        const { currentQuestion } = this.props.questions
-        console.log('Current question:', currentQuestion)
-        return <div>Current Question</div>
+        const { classes } = this.props
+        const { currentQuestion, questions } = this.props.questions
+
+        if (!currentQuestion) {
+            return (
+                <Grid container justify="center" alignItems="center" className={classes.main}>
+                    <Typography>Loading question...</Typography>
+                </Grid>
+            )
+        }
+
+        const { id, author, optionOne, optionTwo } = questions[currentQuestion]
+
+        return (
+            <Fragment>
+                <div className={classes.main}>
+                    <div className={classNames(classes.option, classes.optionOne)} />
+                    <div className={classNames(classes.option, classes.optionTwo)} />
+                </div>
+                <BottomNavigation showLabels className={classes.footer}>
+                    <BottomNavigationAction
+                        label="Previous"
+                        icon={<FontAwesomeIcon icon={faArrowLeft} />}
+                    />
+                    <BottomNavigationAction
+                        label={author}
+                        icon={<FontAwesomeIcon icon={faUser} />}
+                    />
+                    <BottomNavigationAction
+                        label="Next"
+                        icon={<FontAwesomeIcon icon={faArrowRight} />}
+                    />
+                </BottomNavigation>
+            </Fragment>
+        )
     }
 }
 
@@ -31,4 +91,6 @@ function mapStateToProps({ authedUser, questions, currentQuestion }) {
     }
 }
 
-export default connect(mapStateToProps)(CurrentQuestion)
+const StyledCurrentQuestion = withStyles(styles)(CurrentQuestion)
+
+export default connect(mapStateToProps)(StyledCurrentQuestion)
