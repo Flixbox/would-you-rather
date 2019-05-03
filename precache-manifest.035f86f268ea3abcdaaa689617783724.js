0602f3ebc5c5d248aa974c9d0b879363c686f38d
Felix <Felix.Tietjen@txs.de>
2019-05-03 09:42:45 +0200

    Updates

---
+++ precache-manifest.035f86f268ea3abcdaaa689617783724.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ca4d9801f361e8d6d4ac06f60adc3c",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "69084ed9b62071d0d84e",
+    "url": "/would-you-rather/static/js/2.77cc1a4b.chunk.js"
+  },
+  {
+    "revision": "b2b5eaf2918087f786a0",
+    "url": "/would-you-rather/static/js/main.c3f94359.chunk.js"
+  },
+  {
+    "revision": "3c0bbf8aecd297c26580",
+    "url": "/would-you-rather/static/js/runtime~main.2bbe9c67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/would-you-rather/precache-manifest.78546cdb4e1faceaf0887fbfe899df65.js"
+  "/would-you-rather/precache-manifest.035f86f268ea3abcdaaa689617783724.js"
 );
 
 self.addEventListener('message', (event) => {
