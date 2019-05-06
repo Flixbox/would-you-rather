e533089a9e4bfa9bc8ba40e21a3e0d526c013a70
Felix <Felix.Tietjen@txs.de>
2019-05-06 09:23:34 +0200

    feat: moved question into its own component in order to allow rendering of specific questions

--- src/views/CurrentQuestion.js
+++ src/views/CurrentQuestion.js
@@ -2,42 +2,15 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { handleRetrieveNewQuestion, handleQuestionAnswer } from '../actions/questions'
 
-import Option from '../components/Option'
+import Question from '../views/Question'
 
-import { navHeight } from '../helpers/theme'
-import { withStyles } from '@material-ui/core/styles'
-import {
-    Typography,
-    Button,
-    Avatar,
-    Grid,
-    BottomNavigation,
-    BottomNavigationAction,
-} from '@material-ui/core'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft, faUser } from '@fortawesome/free-solid-svg-icons'
-
-const styles = {
-    main: {
-        // The height of the two navbars and the heading is 3xnavHeight
-        height: `calc(100% - ${navHeight * 3}px)`,
-    },
-    option: {
-        flex: '1 0 auto',
-        width: '50%',
-        height: `100%`,
-    },
-    questionContainer: {
-        width: '100%',
-        padding: '5px',
-        height: `${navHeight}px`,
-    },
-    icon: {
-        marginBottom: '5px',
-    },
-}
-
+/**
+ * This helper view retrieves the current logged in user's question.
+ * This question should be the first unanswered one.
+ *
+ * @class CurrentQuestion
+ * @extends {Component}
+ */
 class CurrentQuestion extends Component {
     componentDidMount = () => {
         const { currentQuestion } = this.props.questions
@@ -53,66 +26,10 @@ class CurrentQuestion extends Component {
         dispatch(handleRetrieveNewQuestion(authedUser))
     }
 
-    handleQuestionAnswer = option => {
-        const { dispatch, authedUser } = this.props
+    render() {
         const { currentQuestion } = this.props.questions
 
-        dispatch(handleQuestionAnswer({ authedUser, qid: currentQuestion, answer: option }))
-    }
-
-    render() {
-        const { classes, authedUser } = this.props
-        const { currentQuestion, questions } = this.props.questions
-
-        if (!currentQuestion) {
-            return (
-                <Grid container justify="center" alignItems="center" className={classes.main}>
-                    <Typography>Loading question...</Typography>
-                </Grid>
-            )
-        }
-
-        const { id, author, optionOne, optionTwo } = questions[currentQuestion]
-
-        return (
-            <Fragment>
-                <div className={classes.questionContainer}>
-                    <Typography variant="h3" align="center">
-                        Would you rather...
-                    </Typography>
-                </div>
-                <Grid container className={classes.main}>
-                    <Option
-                        {...optionOne}
-                        option="optionOne"
-                        handleQuestionAnswer={this.handleQuestionAnswer}
-                        authedUser={authedUser}
-                        opposite={optionTwo}
-                    />
-                    <Option
-                        {...optionTwo}
-                        option="optionTwo"
-                        handleQuestionAnswer={this.handleQuestionAnswer}
-                        authedUser={authedUser}
-                        opposite={optionOne}
-                    />
-                </Grid>
-                <BottomNavigation showLabels>
-                    <BottomNavigationAction
-                        label="Previous"
-                        icon={<FontAwesomeIcon icon={faArrowLeft} className={classes.icon} />}
-                    />
-                    <BottomNavigationAction
-                        label={author}
-                        icon={<FontAwesomeIcon icon={faUser} className={classes.icon} />}
-                    />
-                    <BottomNavigationAction
-                        label="Next"
-                        icon={<FontAwesomeIcon icon={faArrowRight} className={classes.icon} />}
-                    />
-                </BottomNavigation>
-            </Fragment>
-        )
+        return <Question question={currentQuestion} />
     }
 }
 
@@ -123,6 +40,4 @@ function mapStateToProps({ authedUser, questions }) {
     }
 }
 
-const StyledCurrentQuestion = withStyles(styles)(CurrentQuestion)
-
-export default connect(mapStateToProps)(StyledCurrentQuestion)
+export default connect(mapStateToProps)(CurrentQuestion)
---
+++ src/views/Question.js
@@ -0,0 +1,120 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { handleRetrieveNewQuestion, handleQuestionAnswer } from '../actions/questions'
+
+import Option from '../components/Option'
+
+import { navHeight } from '../helpers/theme'
+import { withStyles } from '@material-ui/core/styles'
+import {
+    Typography,
+    Button,
+    Avatar,
+    Grid,
+    BottomNavigation,
+    BottomNavigationAction,
+} from '@material-ui/core'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const styles = {
+    main: {
+        // The height of the two navbars and the heading is 3xnavHeight
+        height: `calc(100% - ${navHeight * 3}px)`,
+    },
+    option: {
+        flex: '1 0 auto',
+        width: '50%',
+        height: `100%`,
+    },
+    questionContainer: {
+        width: '100%',
+        padding: '5px',
+        height: `${navHeight}px`,
+    },
+    icon: {
+        marginBottom: '5px',
+    },
+}
+
+/**
+ * This view renders a question and its data.
+ * The prop "question" is the ID of the question that should be rendered.
+ *
+ * @class Question
+ * @extends {Component}
+ */
+class Question extends Component {
+    handleQuestionAnswer = option => {
+        const { dispatch, authedUser, question } = this.props
+
+        dispatch(handleQuestionAnswer({ authedUser, qid: question, answer: option }))
+    }
+
+    render() {
+        const { classes, authedUser, question } = this.props
+        const { questions } = this.props.questions
+
+        if (!question) {
+            return (
+                <Grid container justify="center" alignItems="center" className={classes.main}>
+                    <Typography>Loading question...</Typography>
+                </Grid>
+            )
+        }
+
+        const { id, author, optionOne, optionTwo } = questions[question]
+
+        return (
+            <Fragment>
+                <div className={classes.questionContainer}>
+                    <Typography variant="h3" align="center">
+                        Would you rather...
+                    </Typography>
+                </div>
+                <Grid container className={classes.main}>
+                    <Option
+                        {...optionOne}
+                        option="optionOne"
+                        handleQuestionAnswer={this.handleQuestionAnswer}
+                        authedUser={authedUser}
+                        opposite={optionTwo}
+                    />
+                    <Option
+                        {...optionTwo}
+                        option="optionTwo"
+                        handleQuestionAnswer={this.handleQuestionAnswer}
+                        authedUser={authedUser}
+                        opposite={optionOne}
+                    />
+                </Grid>
+                <BottomNavigation showLabels>
+                    <BottomNavigationAction
+                        label="Previous"
+                        icon={<FontAwesomeIcon icon={faArrowLeft} className={classes.icon} />}
+                    />
+                    <BottomNavigationAction
+                        label={author}
+                        icon={<FontAwesomeIcon icon={faUser} className={classes.icon} />}
+                    />
+                    <BottomNavigationAction
+                        label="Next"
+                        icon={<FontAwesomeIcon icon={faArrowRight} className={classes.icon} />}
+                    />
+                </BottomNavigation>
+            </Fragment>
+        )
+    }
+}
+
+function mapStateToProps({ authedUser, questions }) {
+    return {
+        authedUser,
+        questions,
+    }
+}
+
+const StyledQuestion = withStyles(styles)(Question)
+
+export default connect(mapStateToProps)(StyledQuestion)
