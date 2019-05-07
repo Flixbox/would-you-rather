9225ec27fcb5b16e97f9482131070dc77d525f8f
Felix <Felix.Tietjen@txs.de>
2019-05-07 12:51:21 +0200

    fix: removed unnecessary api function for question formatting

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,10 +1,4 @@
-import {
-    _getUsers,
-    _getQuestions,
-    _saveQuestion,
-    _saveQuestionAnswer,
-    formatQuestion,
-} from './_DATA.js'
+import { _getUsers, _getQuestions, _saveQuestion, _saveQuestionAnswer } from './_DATA.js'
 
 export function getInitialData() {
     return Promise.all([_getUsers(), _getQuestions()]).then(([users, questions]) => ({
@@ -19,8 +13,4 @@ export function saveQuestion(question) {
 
 export function saveQuestionAnswer({ authedUser, qid, answer }) {
     return _saveQuestionAnswer({ authedUser, qid, answer })
-}
-
-export function formatNewQuestion(question) {
-    return formatQuestion(question)
 }
