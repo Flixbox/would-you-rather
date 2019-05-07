b323a78038b9841b5d689187af369adc49150216
Felix <Felix.Tietjen@txs.de>
2019-05-07 11:28:55 +0200

    feat: added controlled input fields to new question form

--- src/views/NewQuestion.js
+++ src/views/NewQuestion.js
@@ -1,7 +1,64 @@
 import React, { Component } from 'react'
 
-export default class NewQuestion extends Component {
+import { Grid, TextField, Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+    main: {
+        width: '100%',
+        margin: 0,
+    },
+}
+
+class NewQuestion extends Component {
+    state = {
+        optionOne: '',
+        optionTwo: '',
+    }
+
+    updateOption = (option, text) => {
+        this.setState(prevState => {
+            return { ...prevState, [option]: text }
+        })
+    }
+
     render() {
-        return <div>NewQuestion</div>
+        const { classes } = this.props
+        return (
+            <Grid container justify="center" spacing={24} className={classes.main}>
+                <Grid item xs={10}>
+                    <Typography variant="h3" align="center">
+                        Would you rather...
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={10}>
+                    <TextField
+                        required
+                        id="optionOne"
+                        name="optionOne"
+                        label="Option 1"
+                        fullWidth
+                        autoComplete="optionOne"
+                        onChange={e => this.updateOption('optionOne', e.target.value)}
+                        value={this.state.optionOne}
+                    />
+                </Grid>
+                <Grid item xs={10}>
+                    <TextField
+                        required
+                        id="optionTwo"
+                        name="optionTwo"
+                        label="Option 2"
+                        fullWidth
+                        autoComplete="optionTwo"
+                        onChange={e => this.updateOption('optionTwo', e.target.value)}
+                        value={this.state.optionTwo}
+                    />
+                </Grid>
+            </Grid>
+        )
     }
 }
+
+export default withStyles(styles)(NewQuestion)
